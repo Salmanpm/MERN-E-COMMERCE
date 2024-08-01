1de07e93cfb5d564c79197cd151db0511449eccd
Salman <146881342+Salmanpm@users.noreply.github.com>
2024-08-01 17:08:12 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://e-commerce-backend-ocdj.onrender.com",
+  base: "http://localhost:4000",
 })
